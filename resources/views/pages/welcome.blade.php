78bc9b199677e4b399972a2d694b2b6de2b97a41
Antonio-Ribezzo <ribezzo.antonio@yahoo.it>
2023-06-13 14:39:54 +0200

    Stampa in pagina delle info dei treni

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -18,8 +18,26 @@
 <body>
 
     <main class="bg-light">
-        
-    </main>
+            <div class="row">
+                @foreach ($trains as $el )
+                    <div class="col-3">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">{{ $el->company }}</h4>
+                                <div class="row">
+                                    <div class="col-6">
+                                        <span>{{ $el->departure_station }}</span>
+                                    </div>
+                                    <div class="col-6">
+                                        <span>{{ $el->arrival_station }}</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </main>
 
 </body>
 
