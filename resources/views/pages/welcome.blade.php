2d96444c129a979c1b74365e1b7196d1eec3c602
Antonio-Ribezzo <ribezzo.antonio@yahoo.it>
2023-06-13 14:58:02 +0200

    Upload Style (Layouts-Pages-Partials)

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -1,44 +1,30 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.app')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@section('titlePage') 
+    Laravel | Migration-Seeder
+@endsection
 
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    <!-- Styles -->
-    @vite('resources/js/app.js')
-
-</head>
-
-<body>
+@section('contentHome') 
 
     <main class="bg-light">
-            <div class="row">
-                @foreach ($trains as $el )
-                    <div class="col-3">
-                        <div class="card">
-                            <div class="card-body">
-                                <h4 class="card-title">{{ $el->company }}</h4>
-                                <div class="row">
-                                    <div class="col-6">
-                                        <span>{{ $el->departure_station }}</span>
-                                    </div>
-                                    <div class="col-6">
-                                        <span>{{ $el->arrival_station }}</span>
-                                    </div>
+        <div class="row">
+            @foreach ($trains as $el )
+                <div class="col-3">
+                    <div class="card">
+                        <div class="card-body">
+                            <h4 class="card-title">{{ $el->company }}</h4>
+                            <div class="row">
+                                <div class="col-6">
+                                    <span>{{ $el->departure_station }}</span>
+                                </div>
+                                <div class="col-6">
+                                    <span>{{ $el->arrival_station }}</span>
                                 </div>
                             </div>
                         </div>
                     </div>
-                @endforeach
-            </div>
-        </main>
-
-</body>
-
-</html>
+                </div>
+            @endforeach
+        </div>
+    </main>
+@endsection
